cd0298e22c1963839e26424f16d95c8125c5b78f
andieelmes <andie.elmes@gmail.com>
2018-08-06 15:40:44 +0300

    add: remove old js

--- src/js/requestAnimationFramePolyfill.js
+++
@@ -1,6 +0,0 @@
-window.raf = (() => {
-  return window.requestAnimationFrame ||
-    window.webkitRequestAnimationFrame ||
-    window.mozRequestAnimationFrame ||
-    function(callback) {setTimeout(callback, 1000 / 60)}
-})()
